85d1296b3f60eec517573934805e93110a0ffb7d
CameronPC <cameronspixels@gmail.com>
2018-02-26 18:20:03 +0000

    loading should now work

--- loading.js
+++ loading.js
@@ -1,9 +1,1 @@
-var grid = document.getElementById("griddy");
-var loadAnimation = document.getElementById("loading");
-grid.className += "js-loading";
-window.addEventListener("load", showPage, false);
-
-function showPage() {
-  grid.className = grid.className.replace("js-loading", " ");
-  loadAnimation.className += "hide";
-}
+window.onload = function () { alert("It's loaded!") }
