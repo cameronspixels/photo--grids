7cd80c7f10343416d04322bccaaf2a8fa5fd42f8
cameronspixels <cameronspixels@gmail.com>
2018-03-06 15:32:53 +0000

    Javascipt adds a new item on button click.

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ window.onload = function () {
   document.getElementById("the--grid").classList.remove('content--loading');
   document.getElementById("loading").classList.add('hide');
 }
+
+function removeHidden() {
+  document.getElementById("classHiding").classList.remove('hidden');
+}
