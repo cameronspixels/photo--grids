17918a0f234ca965beb468b767e666af6796d7cb
cameronspixels <cameronspixels@gmail.com>
2018-03-13 17:45:05 +0000

    Files.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
-window.onload = function () {
+window.onload = function() {
   document.getElementById("the--grid").classList.remove('content--loading');
   document.getElementById("loading").classList.add('hide');
 }
 
+var firstHidden = document.getElementById('imageHiding1');
+
+var secondHidden = document.getElementById('imageHiding2');
+
+var thirdHidden = document.getElementById('imageHiding3');
+
 function removeHidden() {
-  document.getElementById("classHiding").classList.remove('hidden');
+
+  if (firstHidden.classList.contains('hidden')) {
+
+    firstHidden.classList.remove('hidden');
+
+  } else if (secondHidden.classList.contains('hidden')) {
+
+    secondHidden.classList.remove('hidden');
+
+  } else if (thirdHidden.classList.contains('hidden')) {
+
+    thirdHidden.classList.remove('hidden');
+
+  }
+
 }
